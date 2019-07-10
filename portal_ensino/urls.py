48cadfae119a4fd00bbedd3c7fb949d73f0594f9
HLAM <hemilioaraujo@gmail.com>
2019-07-10 12:30:35 -0300

    removido url path de urls.py(usado como teste)

--- portal_ensino/urls.py
+++ portal_ensino/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello),
 ]
