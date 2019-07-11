6aa625b151491b1d82b5d7acd7a23bd5d4d809b1
HLAM <hemilioaraujo@gmail.com>
2019-07-10 22:32:18 -0300

    Adicionado campos data_nascimento e id na classe Usuario e transferido login.html para templates/registration

--- portal_ensino/usuarios/models.py
+++ portal_ensino/usuarios/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=30)
     sobrenome = models.CharField(max_length=30)
     instituicao = models.CharField(max_length=30)
+    data_nascimento = models.DateField(default='1970-01-01')
     bio = models.TextField()
 
 
