1434288ca4a042e29c397506e7d06f85044253f0
HLAM <hemilioaraujo@gmail.com>
2019-07-15 23:31:59 -0300

    comentado trecho que estava causando erro e finalizado os labels do cadastro

--- portal_ensino/usuarios/forms.py
+++ portal_ensino/usuarios/forms.py
@@ -6,10 +6,24 @@ from .models import Profile
 
 
 class UserForm(UserCreationForm):
+
     email = forms.EmailField(required=True, label='E-mail:')
     first_name = forms.CharField(max_length=30, label='Nome:')
     last_name = forms.CharField(max_length=30, label='Sobrenome:')
     username = forms.CharField(max_length=30, label='Usuário:')
+
+    password1 = forms.CharField(
+        label="Senha:",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
+    password2 = forms.CharField(
+        label="Confirmar senha:",
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+
 
     class Meta:
         model = User
@@ -24,12 +38,14 @@ class UserForm(UserCreationForm):
             'password': forms.PasswordInput(attrs={'maxlength': 30})
         }
 
-    def save(self, commit=True):
+
+# CAUSANDO ERRO
+    '''def save(self, commit=True):
         user = super(UserForm, self).save(commit=False)
         user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
-        return user
+        return user'''
 
 
 class ProfileForm(ModelForm):
