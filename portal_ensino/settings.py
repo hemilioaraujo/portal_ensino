d2e06777bfbae2c468203773515d74fc7a7850f0
HLAM <hemilioaraujo@gmail.com>
2019-07-18 16:55:26 -0300

    adicionando variáveis para email no arquivo settings.py

--- portal_ensino/settings.py
+++ portal_ensino/settings.py
@@ -133,3 +133,15 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = config('EMAIL_USER')
+
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
