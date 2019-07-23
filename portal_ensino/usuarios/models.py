38e4f0d8dc924955f2ead3252a80257c9e4695d1
HLAM <hemilioaraujo@gmail.com>
2019-07-23 16:56:19 -0300

    adicionada função save no model de profile para limitar o tamanho da imagem do usuário

--- portal_ensino/usuarios/models.py
+++ portal_ensino/usuarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,4 +12,14 @@ class Profile(models.Model):
     foto = models.ImageField(upload_to='fotos/profile', default='fotos/profile/default.jpeg', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} {self.user.email} Profile'
+        return f'Usuário: {self.user.username} Email: {self.user.email}'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.foto.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.foto.path)
