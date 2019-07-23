439b23a7a48ed609e646d5cde38659fd107a22b8
HLAM <hemilioaraujo@gmail.com>
2019-07-23 19:47:07 -0300

    adicionada função em usuarios/models.py para salvar imagem com o nome de usuario

--- portal_ensino/usuarios/models.py
+++ portal_ensino/usuarios/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+import os
+
+
+def renomear_imagem(instance, filename):
+    upload_to = 'fotos/profile/'
+    ext = filename.split('.')[-1]
+
+    filename = f'{instance.user}.{ext}'
+
+    return os.path.join(upload_to, filename)
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -9,7 +20,7 @@ class Profile(models.Model):
     instituicao = models.CharField(max_length=30)
     data_nascimento = models.DateField(default='1970-01-01')
     bio = models.TextField(blank=True)
-    foto = models.ImageField(upload_to='fotos/profile', default='fotos/profile/default.jpeg', null=True, blank=True)
+    foto = models.ImageField(upload_to=renomear_imagem, default='fotos/profile/default.jpeg', null=True, blank=True)
 
     def __str__(self):
         return f'Usuário: {self.user.username} Email: {self.user.email}'
