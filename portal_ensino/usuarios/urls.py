c83e27d049d6eee0d821ce58029daeda289006cc
HLAM <hemilioaraujo@gmail.com>
2019-07-23 16:12:19 -0300

    adicionado o path atualizar-usuario

--- portal_ensino/usuarios/urls.py
+++ portal_ensino/usuarios/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .views import usuarios_list, usuarios_novo, do_logout, do_login
+from .views import usuarios_list, usuarios_novo, do_logout, do_login, usuarios_update
 
 
 urlpatterns = [
     path('list/', usuarios_list, name='lista_usuarios'),
     path('novo/', usuarios_novo, name="novo_usuario"),
+    path('atualizar-usuario/', usuarios_update, name="atualizar_usuario"),
     path('logout/', do_logout, name="logout"),
     path('login/', do_login, name="login"),
 
