13edc36658a6a50707e65927eaab8ea09d318ef9
HLAM <hemilioaraujo@gmail.com>
2019-12-16 19:34:17 -0300

    add model Aulas

--- portal_ensino/aulas/models.py
+++ portal_ensino/aulas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Aulas(models.Model):
+    titulo = models.TextField(blank=False, null=False)
+    link = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.id} - {self.titulo}'
