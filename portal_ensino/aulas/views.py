42fd0e2fb0193b86f43f86aa2f33cb3b623f0ae6
HLAM <hemilioaraujo@gmail.com>
2019-12-18 22:32:00 -0300

    adicionado o template de aula, url aula,  view aula e include no url principal

--- portal_ensino/aulas/views.py
+++ portal_ensino/aulas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+@login_required
+def aula(request):
+    return render(request, 'aula.html',{'usuario': request.user})
--- portal_ensino/urls.py
+++ portal_ensino/urls.py
@@ -19,11 +19,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import home
 from .usuarios import urls as urls_usuarios
+from .aulas import urls as urls_aulas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
 
     # redirecionapara app usuarios
-    path('usuarios/', include(urls_usuarios))
+    path('usuarios/', include(urls_usuarios)),
+
+    # redirecionapara app aulas
+    path('aulas/', include(urls_aulas)),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # possibilita visualizar imagens dos profiles
