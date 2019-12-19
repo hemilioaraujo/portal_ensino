b37c6f7e91b4ecdc17c49c3d333492506e9152d6
HLAM <hemilioaraujo@gmail.com>
2019-12-18 23:37:53 -0300

    adicionado static para o servidor de testes servir os arquivos estáticos

--- portal_ensino/aulas/urls.py
+++ portal_ensino/aulas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import aula
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('aula/', aula, name='aula'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
