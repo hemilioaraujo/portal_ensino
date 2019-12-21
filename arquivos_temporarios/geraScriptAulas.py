05d512c0010d5e930d6fbb838b68a530ae64e665
HLAM <hemilioaraujo@gmail.com>
2019-12-20 23:59:00 -0300

    alterado o script para gerar somente o id do vídeo

--- arquivos_temporarios/geraScriptAulas.py
+++ arquivos_temporarios/geraScriptAulas.py
@@ -7,7 +7,15 @@ textoDestino = []
 
 for linha in texto:
     id, titulo, link = linha.split(';')
-    string = f"Aulas.objects.create(\n\tid={id},\n\ttitulo='{titulo}',\n\tlink='{link[:-1]}')\n"
+
+    posicao = link.find('=') + 1
+    a = link[posicao:]
+    posicao = a.find('&')
+    link = a[:posicao]
+
+    print(link)
+
+    string = f"Aulas.objects.create(\n\tid={id},\n\ttitulo='{titulo}',\n\tlink='{link}')\n"
     textoDestino.append(string)
 
 destino.writelines(textoDestino)
