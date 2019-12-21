403d47d45d803ad94e6961dd19abf63f556ef9ba
HLAM <hemilioaraujo@gmail.com>
2019-12-21 00:02:06 -0300

    adicionados as urls e funções para páginas anteriores e posteriores

--- portal_ensino/aulas/urls.py
+++ portal_ensino/aulas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import aula
+from .views import aula, aula_anterior, proxima_aula
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('aula/', aula, name='aula'),
+    path('proxima_aula/', proxima_aula, name='proxima_aula'),
+    path('aula_anterior/', aula_anterior, name='aula_anterior'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portal_ensino/aulas/views.py
+++ portal_ensino/aulas/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from portal_ensino.usuarios.models import Profile
+from portal_ensino.aulas.models import Aulas
 
 
 # Create your views here.
 @login_required
 def aula(request):
     return render(request, 'aula.html',{'usuario': request.user})
+
+
+@login_required
+def aula_anterior(request):
+    pass
+
+
+@login_required
+def proxima_aula(request):
+    usuario = Profile.objects.get(user_id=request.user.id)
+    aula_id = request.user.profile.aula_atual.id
+    usuario.aula_atual = Aulas.objects.get(id=aula_id+1)
+    usuario.save()
+    # print(request.user.profile.aula_atual.id)
+    return render(request, 'aula.html', {'usuario': request.user})
+
+
+@login_required
+def aula_anterior(request):
+    usuario = Profile.objects.get(user_id=request.user.id)
+    aula_id = request.user.profile.aula_atual.id
+    usuario.aula_atual = Aulas.objects.get(id=aula_id-1)
+    usuario.save()
+    # print(request.user.id)
+    return render(request, 'aula.html', {'usuario': request.user})
