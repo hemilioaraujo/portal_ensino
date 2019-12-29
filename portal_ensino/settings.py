455c07152cdd994f50c7fbb92d359ec24f837614
WhoisBsa <mbscbsjbs@gmail.com>
2019-12-29 16:45:28 -0300

    Setar root dos arquivos estaticos

--- portal_ensino/settings.py
+++ portal_ensino/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [
     'estaticos',
