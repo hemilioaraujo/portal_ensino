de308cd6945dd3d10f88b0d6493cf123ebba6127
HLAM <hemilioaraujo@gmail.com>
2020-10-08 00:29:27 -0300

    adicinada função sobre para renderizar a página sobre.html

--- portal_ensino/views.py
+++ portal_ensino/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 
 def home(request):
-   return render(request, 'index.html')
+    return render(request, 'index.html')
+
+
+def sobre(request):
+    return render(request, 'sobre.html')
