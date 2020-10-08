58a0052d236e33bf1e6859d04bea5bf8ceae0130
HLAM <hemilioaraujo@gmail.com>
2020-10-08 00:28:41 -0300

    adicionado pattern para página sobre

--- portal_ensino/urls.py
+++ portal_ensino/urls.py
@@ -1,29 +1,15 @@
-"""portal_ensino URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from .views import home, sobre
 from .usuarios import urls as urls_usuarios
 from .aulas import urls as urls_aulas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('sobre/', sobre, name='sobre'),
 
     # redirecionapara app usuarios
     path('usuarios/', include(urls_usuarios)),
