55fb36e3b5485b48da6bdb996b3a9ca8cb6312b5
HLAM <hemilioaraujo@gmail.com>
2020-12-05 12:33:15 -0300

    configurado settings.DATABASE e instalado psycopg2-binary

--- portal_ensino/settings.py
+++ portal_ensino/settings.py
@@ -78,9 +78,25 @@ WSGI_APPLICATION = 'portal_ensino.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'portal_ensino',
+
+        'USER': 'hemilio',
+
+        'PASSWORD': 'H13l09a88M',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
