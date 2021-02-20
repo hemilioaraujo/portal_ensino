12ac8da91b4e96e1657f7d402c9b4ca122ec98b3
HLAM <hemilioaraujo@gmail.com>
2021-02-20 10:15:05 -0300

    adicionadas variaveis de send email

--- portal_ensino/settings.py
+++ portal_ensino/settings.py
@@ -178,3 +178,11 @@ if AWS_ACCESS_KEY_ID:
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+# EMAIL
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
